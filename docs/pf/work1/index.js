4a81c8b866fcae1664444eb3415151bf93eb9798
schim613 <uximin@naver.com>
2021-08-24 03:20:58 +0900

    연락페이지 테스트

--- docs/pf/work1/index.js
+++ docs/pf/work1/index.js
@@ -2,3 +2,25 @@
 new fullpage('#fullpage', {
   menu: ".top-bar .menu-box-1"
 });
+
+// 연락-이메일 폼
+function sendEmailForm(form) {
+  if ( form._replyto.value.length == 0 ) {
+    alert('이메일 주소를 입력해주세요.');
+    form._replyto.focus();
+    return;
+  }
+  
+  if ( form.message.value.length == 0 ) {
+    alert('메세지를 입력해주세요.');
+    form.message.focus();
+    return;
+  }
+  
+  form.submit();
+  
+  form._replyto.value = '';
+  form.message.value = '';
+  form.submit1.innerHTML = 'DONE!';
+  form.submit1.disabled = true;
+}
